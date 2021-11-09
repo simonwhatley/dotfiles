5d91faf6529420982138c6338bc38a7c45ac2927
Simon Whatley <simonwhatley@gmail.com>
2021-11-09 20:53:50 +0000

    Add homebrew setup script

--- homebrew/setup.sh
+++ homebrew/setup.sh
@@ -1,1 +1,14 @@
 #! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../utils.sh
+
+COMMENT=\#*
+
+sudo -v
+
+info "Installing Brewfile packages..."
+brew bundle
+success "Finished installing Brewfile packages."
