6072562d996a3703f87460817321f9ecef28815a
Simon Whatley <simonwhatley@gmail.com>
2021-11-09 20:54:18 +0000

    Update git setup

--- git/setup.sh
+++ git/setup.sh
@@ -1,1 +1,18 @@
 #! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../utils.sh
+
+SOURCE="$(realpath -m .)"
+DESTINATION="$(realpath -m ~)"
+
+info "Configuraing git..."
+
+find . -name ".git*" | while read fn; do
+  fn=$(basename $fn)
+  # symlink "$SOURCE/$fn" "$DESTINATION/$fn"
+done
+
+success "Finished configuring git."
