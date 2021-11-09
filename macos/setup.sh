584461520d2f15dd678c981185a7cd048c5183a0
Simon Whatley <simonwhatley@gmail.com>
2021-11-09 20:55:11 +0000

    Update macos setup script

--- macos/setup.sh
+++ macos/setup.sh
@@ -1,1 +1,21 @@
 #! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../utils.sh
+
+SOURCE="$(realpath -m .)"
+DESTINATION="$(realpath -m ~)"
+
+info "Configuraing macOS defaults..."
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
